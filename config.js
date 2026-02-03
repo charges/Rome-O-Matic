cb06e9f52823e076b978175283bf21a7bf159c61
Maynard G. Krebs <chrisharges@gmail.com>
2026-02-03 10:42:42 -0800

    Fix backendBase URL casing

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const APP_CONFIG = {
   appName: "Rome-O-Matic",
-  backendBase: "https://Rome-O-Matic-Backend.onrender.com"
+  backendBase: "https://rome-o-matic-backend.onrender.com"
 };
 
 const API_URL = `${APP_CONFIG.backendBase}/api/articles`;
